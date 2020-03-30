d3d3707da1b8677334a5dec8f0d29822cce17c70
David <41311088+biagiola@users.noreply.github.com>
2020-03-30 14:30:27 -0300

    Add files via upload

--- java/com/example/flickerbrowser/FlickrRecyclerViewAdapter.java
+++ java/com/example/flickerbrowser/FlickrRecyclerViewAdapter.java
@@ -2,7 +2,9 @@ package com.example.flickerbrowser;
 
 import android.content.Context;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -19,6 +21,35 @@ class FlickrRecyclerViewAdapter extends RecyclerView.Adapter<FlickrRecyclerViewA
     public FlickrRecyclerViewAdapter(Context context, List<Photo> photosList) {
         mContext = context;
         mPhotosList = photosList;
+    }
+
+    @NonNull
+    @Override
+    public FlickrImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // Called by the layout manager when it needs a new view
+        Log.d(TAG, "onCreateViewHolder: new view requested");
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.browse, parent, false);
+        return new FlickrImageViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FlickrImageViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        Log.d(TAG, "getItemCount: called");
+        return ((mPhotosList != null) && (mPhotosList.size() !=0) ? mPhotosList.size() : 0);
+    }
+
+    void loadNewData(List<Photo> newPhotos) {
+        mPhotosList = newPhotos;
+        notifyDataSetChanged();
+    }
+
+    public Photo getPhoto(int position) {
+        return ((mPhotosList != null) && (mPhotosList.size() != 0) ? mPhotosList.get(position) : null);
     }
 
     static class FlickrImageViewHolder extends RecyclerView.ViewHolder {
